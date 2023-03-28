8e69fe311243b88ffcfdbd3c86187761dfde011c
CopitoFrostbite <roberto_ppwp@hotmail.com>
2023-03-27 19:38:53 -0600

    modificacion a usuarios

--- WebServices/registro.php
+++ WebServices/registro.php
@@ -1,9 +1,9 @@
 <?php
+
 $servername = "localhost";
 $username = "root";
 $password = "miau123";
 $dbname = "bdm";
-
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
--- spGestionUsuarios.sql
+++ spGestionUsuarios.sql
@@ -11,21 +11,21 @@ CREATE PROCEDURE spGestionUsuarios(
     IN Ap VARCHAR(30),
     IN Am VARCHAR(30),
     IN Pass VARCHAR(30),
-    IN Rol BOOLEAN,
+    IN Rol INT,
     IN Imagen BLOB,
     IN Genero BOOLEAN,
 	IN Correo VARCHAR(30),
     IN Fecha Date, 
     IN Fechan Date,
-    IN err BOOLEAN
+    IN err INT
     
 )
 BEGIN
     IF Accion = 'IN' THEN
         SET Fecha = NOW();
         SET Imagen = null;
-        INSERT INTO Usuarios(ID_usuario, Nombre, Ap, Am, Pass, Rol, Imagen, Genero, Correo, Fecha, Fechan, err )
-        VALUES(0, Nombre, Ap, Am, Pass, 1, null, 1, Correo, '2021-05-25', '2021-05-25',0 );
+        INSERT INTO Usuarios(Nombre, Ap, Am, Pass, Rol, Imagen, Genero, Correo, Fecha, Fechan, err )
+        VALUES( Nombre, Ap, Am, Pass, Rol, 1, Genero, Correo, Fecha, Fechan,0 );
     END IF;
     
     IF Accion = 'UP' THEN
