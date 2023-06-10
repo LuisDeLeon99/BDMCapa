887784c645524a9eae8d146d28debdf1d00e2b55
CopitoFrostbite <roberto_ppwp@hotmail.com>
2023-06-10 05:23:04 -0600

    agregue muy pocas cosas, cunada chan solo lloraba

--- WebServices/traeCurso.php
+++ WebServices/traeCurso.php
@@ -4,7 +4,16 @@ require_once 'conexion.php';
 
 if (!isset($_SESSION['usuario'])) {
     // No hay sesión, redirigir al usuario a otra página o mostrar un mensaje de error
-    http_response_code(302);
+    
+    $response = array(
+        'error' => true,
+        'message' => 'No hay sesión iniciada',
+        'redirect_url' => 'http://localhost:3005/BDMCapa/pages/Nologin.html' // Cambia 'ruta-de-tu-página-de-redirección.html' con la URL de la página a la que deseas redirigir
+    );
+
+    http_response_code(200); // Establecer el código de respuesta HTTP como 200 (OK)
+    header('Content-Type: application/json'); // Establecer el encabezado de respuesta como JSON
+    echo json_encode($response); // Devolver la respuesta JSON     
     exit();
 }
 
