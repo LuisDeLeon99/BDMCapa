b9a19015e6773f6df4bf13aa551379c574b74bc8
CopitoFrostbite <roberto_ppwp@hotmail.com>
2023-06-13 05:44:54 -0600

    en el progreso falta que hacer si llega a 100

--- WebServices/siguientenivel.php
+++ WebServices/siguientenivel.php
@@ -19,15 +19,34 @@ if (!isset($_SESSION['usuario'])) {
     $ID_usuario = intval($_SESSION['ID_usuario']);
     $cursoID = isset($_POST['cursoId']) ? intval($_POST['cursoId']) : 0;
     $nivelID = isset($_POST['nivelId']) ? intval($_POST['nivelId']) : 0;
+    $nivel = isset($_POST['nivel']) ? intval($_POST['nivel']) : 0;
     $Creacion = '2000-05-05';
+    $totalCosto = 0;
+    $totalCostoDecimal = number_format($totalCosto, 2, '.', '');
    // Consulta para llamar al procedimiento almacenado según la acción y la paginación
    
    $completado = 0;
    $query = "CALL spGestionNivelesUsuarios('UP','$ID_usuario','$cursoID','$nivelID','$completado','$Creacion')";
    $result = $conn->query($query);
    
+   $query2 = "CALL spGestionCursosUsuarios('SE5','$cursoID','$ID_usuario','1','$totalCostoDecimal','','$Creacion','$Creacion')";
+   $result2 = $conn->query($query2);
+    $row2 = $result2->fetch_assoc(); // Obtener la fila de resultados
+
+    $avance = $row2['NivelAvance'];
+    $maxniv = $row2['NumeroNiveles']; // Obtener el valor de NivelAvance
+
+    $nivelID++; // Incrementar nivelId
+    $nivel++;
+    $response = array(
+        'cursoId' => $cursoID,
+        'nivelId' => $nivelID,
+        'progreso' => $avance,
+        'nivelmax' => $maxniv,
+        'nivel' => $nivel
+    );
     header('Content-Type: application/json');
-    echo json_encode('hecho');
+    echo json_encode($response);
    
     
 
